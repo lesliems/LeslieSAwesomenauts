863e4ec462b2d43870b7e9663bdfaf9d61b07457
leslie Segovia <lesliemsegovia@gmail.com>
2015-03-10 09:04:28 -0700

    1

---
+++ js/entities/entities.js
@@ -0,0 +1,51 @@
+game.PlayerEntity = me.Entity.extend({
+    //constructor
+    init: function(x, y, settingd) {
+        this._super(me.Entity, 'init', [x, y, {
+                image: "player",
+                width: 64,
+                height: 64,
+                //size of the image
+                spritewidth: "64",
+                spriteheight: "64",
+                getShape: function() {
+                    return(new me.Rect(0, 0, 64, 64)).toPolygon();
+                }
+            }]);
+
+        //velocity for player
+        this.body.setVelocity(5, 20);
+
+        this.renderable.addAnimation("idle", [78]);
+        this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+
+        this.renderable.setCurrentAnimation("idle");
+
+    },
+    //delta - change in time
+    update: function(delta) {
+        //check if key was pressed
+        if (me.input.isKeyPressed("right")) {
+            //adds to the position of my x by the velocity defined above in
+            //setVelocity() and multiplying it by me.timer.tick
+            //by me.time.tick makes the movement look smooth
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+            this.flipX(true);
+        } else {
+            this.body.vel.x = 0;
+        }
+
+        if (this.body.vel.x !== 0) {
+            if (!this.renderable.isCurrentAnimation("walk")) {
+                this.renderable.setCurrentAnimation("walk");
+            }
+        }else{
+            this.renderable.setCurrentAnimation("idle");
+        }
+
+        this.body.update(delta);
+
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    }
+});
---
+++ js/screens/play.js
@@ -0,0 +1,33 @@
+game.PlayScreen = me.ScreenObject.extend({
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {
+		// reset the score
+		game.data.score = 0;
+                //teliing it what to look at as far as maps
+                me.levelDirector.loadLevel("level101");
+                
+                //pulling the player and setiing where he will show up
+                var player = me.pool.pull("player", 0, 420, {});
+                //adding the player to the world
+                me.game.world.addChild(player, 5);
+                
+                //bind key for movement
+                me.input.bindKey(me.input.KEY.RIGHT, "right");
+                
+
+		// add our HUD to the game world
+		this.HUD = new game.HUD.Container();
+		me.game.world.addChild(this.HUD);
+	},
+
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		// remove the HUD from the game world
+		me.game.world.removeChild(this.HUD);
+	}
+});
